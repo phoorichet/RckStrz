a4ff3754d357cab884a282118d59a1dd23ae5c86
Phoorichet Thepdusith <phoorichet.thepdusith@sv.cmu.edu>
2012-09-17 15:43:03 -0400

    Init RckStrz Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RckStrz::Application.config.secret_token = '14eb4786b2244a029fac1b6bb18c8b4724b68050cefad01b45bb2c9892d6277bb53d88db634ef3eeb984a51599435d1b9b2f7c071c029a458a937c5f2fee5fb3'
